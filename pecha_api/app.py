147619bed1d6f52c2495c93879b80ca3a8405dfe
lobsam <sanduplobzang@gmail.com>
2026-05-22 14:28:49 +0530

    refactor: clean up app.py and texts_openpecha_views.py by removing unused code and merging conflicting sections

--- pecha_api/app.py
+++ pecha_api/app.py
@@ -40,11 +40,7 @@ from pecha_api.text_uploader.text_metadata import text_metadata_views
 from pecha_api.text_uploader.collections import uploader_collections_views
 from pecha_api.collections import collections_openpecha_views
 from pecha_api.texts import texts_openpecha_views
-<<<<<<< HEAD
-=======
 from pecha_api.texts.segments import segments_openpecha_views
-
->>>>>>> 84761972ac523b2c0d18e6e45f16c08fa0ecc7bc
 from pecha_api.routines import routines_views
 import uvicorn
 
@@ -91,11 +87,7 @@ api.include_router(uploader_collections_views.text_uploader_collections_router)
 api.include_router(routines_views.routines_router)
 api.include_router(collections_openpecha_views.collections_v2_router)
 api.include_router(texts_openpecha_views.texts_v2_router)
-<<<<<<< HEAD
-=======
 api.include_router(segments_openpecha_views.segments_v2_router)
-
->>>>>>> 84761972ac523b2c0d18e6e45f16c08fa0ecc7bc
 
 api.include_router(routines_views.user_routine_router)
 api.add_middleware(
--- pecha_api/texts/texts_openpecha_views.py
+++ pecha_api/texts/texts_openpecha_views.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-from __future__ import annotations
-
-from fastapi import APIRouter
-from starlette import status
-from fastapi import Query
-
-from .texts_openpecha_services import get_text_detail_by_id
-from .text_openpecha_response_models import TextDetailResponse, TextDetailRequest
-
-texts_v2_router = APIRouter(
-    prefix="/v2/texts",
-    tags=["texts-v2"]
-=======
 from typing import Annotated
 
 from fastapi import APIRouter, Query
@@ -26,21 +12,10 @@ from .texts_openpecha_service import (
 texts_v2_router = APIRouter(
     prefix="/v2/texts",
     tags=["texts-v2"],
->>>>>>> 84761972ac523b2c0d18e6e45f16c08fa0ecc7bc
 )
 
 
 @texts_v2_router.get(
-<<<<<<< HEAD
-    "/detail",
-    response_model=TextDetailResponse,
-    status_code=status.HTTP_200_OK,
-    summary="Get a text with pagination",
-    description="Retrieve a text by its OpenPecha ID, including local edition details with pagination."
-)
-async def read_text_by_id(text_id: str, offset: int = Query(default=0), limit: int = Query(default=30)) -> TextDetailResponse:
-    return await get_text_detail_by_id(text_id=text_id, offset=offset, limit=limit)
-=======
     "/collection/{collection_id}",
     status_code=status.HTTP_200_OK,
     summary="Get texts by collection from OpenPecha",
@@ -66,4 +41,3 @@ async def get_texts_by_collection(
 )
 async def get_text_by_id(text_id: str) -> V2TextDTO:
     return await get_text_by_id_from_openpecha(text_id=text_id)
->>>>>>> 84761972ac523b2c0d18e6e45f16c08fa0ecc7bc
