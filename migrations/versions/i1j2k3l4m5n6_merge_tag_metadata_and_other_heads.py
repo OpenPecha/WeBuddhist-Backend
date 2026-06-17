13d27c865f66d0e7de82edd442ef2b309ffaf24a
Lungsangg <lungsang2023@gmail.com>
2026-06-17 23:22:03 +0530

    migration_fixed

--- migrations/versions/i1j2k3l4m5n6_merge_tag_metadata_and_other_heads.py
+++ migrations/versions/i1j2k3l4m5n6_merge_tag_metadata_and_other_heads.py
@@ -1,7 +1,7 @@
 """merge tag_metadata and other heads
 
 Revision ID: i1j2k3l4m5n6
-Revises: d9e8f7a6b5c4, f9a0b1c2d3e5, g0a1b2c3d4e5, h1a2b3c4d5e6
+Revises: d9e8f7a6b5c4, j3c4d5e6f7a8
 Create Date: 2026-06-17 17:49:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "i1j2k3l4m5n6"
-down_revision: Union[str, Sequence[str], None] = ("d9e8f7a6b5c4", "f9a0b1c2d3e5", "g0a1b2c3d4e5", "h1a2b3c4d5e6")
+down_revision: Union[str, Sequence[str], None] = ("d9e8f7a6b5c4", "j3c4d5e6f7a8")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
