6e9b9337d7ab9fde376c7570904c657663b712a9
Lungsangg <lungsang2023@gmail.com>
2026-08-28 11:20:35 +0530

    indentation_add

--- pecha_api/texts/segments/segments_views.py
+++ pecha_api/texts/segments/segments_views.py
@@ -44,17 +44,20 @@ async def get_segment(
 ) -> SegmentDTO:
     return await get_segment_details_by_id(segment_id=segment_id, text_details=text_details)
 
+
 @segment_router.get("/{segment_id}/info", status_code=status.HTTP_200_OK)
 async def get_info_for_segment(
     segment_id: str
 ) -> SegmentInfoResponse:
     return await get_info_by_segment_id(segment_id=segment_id)
 
+
 @segment_router.get("/{segment_id}/root_text", status_code=status.HTTP_200_OK)
 async def get_root_text_for_segment(
     segment_id: str
 ):
     return await get_root_text_mapping_by_segment_id(segment_id=segment_id)
+
 
 @segment_router.get("/{segment_id}/translations", status_code=status.HTTP_200_OK)
 async def get_translations_for_segment(
@@ -63,6 +66,7 @@ async def get_translations_for_segment(
     return await get_translations_by_segment_id(
         segment_id=segment_id
     )
+
 
 @segment_router.get("/{segment_id}/commentaries", status_code=status.HTTP_200_OK)
 async def get_commentaries_for_segment(
