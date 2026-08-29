388edbf947da0dbdbb92e4dcbb069fc04cdd58e4
tenkus47 <tenkus47@gmail.com>
2026-08-29 13:19:17 +0530

    Add migration for merging China restricted items, author group status, and event timezone heads
    - Created a new migration script to merge the specified heads.
    - Added a migration for adding a timezone column to the events table and creating the event_reminders table with necessary constraints and indexes.
    - Introduced tests for the notification SQS client, covering various scenarios for sending event notifications and handling configurations.

--- migrations/versions/evt2a3b4c5d6f_add_event_timezone_and_reminders.py
+++ migrations/versions/evt2a3b4c5d6f_add_event_timezone_and_reminders.py
@@ -1,6 +1,6 @@
 """add timezone column to events and event_reminders table
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: evt2a3b4c5d6f
 Revises: 813cecbdd710
 Create Date: 2026-08-26 00:00:00.000000
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from migrations.idempotency import column_exists, table_exists, index_exists
 
 # revision identifiers, used by Alembic.
-revision: str = "a1b2c3d4e5f6"
+revision: str = "evt2a3b4c5d6f"
 down_revision: Union[str, None] = "813cecbdd710"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
